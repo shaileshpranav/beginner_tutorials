f2042fb4480265807ae01bb3a1f7a693c16d6d5e
Shailesh Pranav Rajendran <shaileshpranav.r@gmail.com>
2022-11-16 18:41:07 -0500

    Commit after checks

--- src/Service.cpp
+++ src/Service.cpp
@@ -1,44 +1,58 @@
-#include "rclcpp/rclcpp.hpp"
-#include "beginner_tutorials/srv/input_str.hpp"                                       
-
+/**
+ * @file Service.cpp
+ * @author Shailesh Pranav Rajendran (spraj@umd.edu)
+ * @brief A service client script
+ * @version 0.1
+ * @date 2022-11-16
+ *
+ * @copyright MIT License (c) 2022 Shailesh Pranav Rajendran
+ *
+ */
 #include <chrono>
 #include <cstdlib>
 #include <memory>
 
+#include "beginner_tutorials/srv/input_str.hpp"
+#include "rclcpp/rclcpp.hpp"
+
 using namespace std::chrono_literals;
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
 
   if (argc == 1) {
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: To change string of publisher");      
-      return 1;
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+                "usage: To change string of publisher");
+    return 1;
   }
 
-  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("service_client");  
-  rclcpp::Client<beginner_tutorials::srv::InputStr>::SharedPtr client =           
-    node->create_client<beginner_tutorials::srv::InputStr>("service_node");         
+  std::shared_ptr<rclcpp::Node> node =
+      rclcpp::Node::make_shared("service_client");
+  rclcpp::Client<beginner_tutorials::srv::InputStr>::SharedPtr client =
+      node->create_client<beginner_tutorials::srv::InputStr>("service_node");
 
-  auto request = std::make_shared<beginner_tutorials::srv::InputStr::Request>();       
-  request->inp_msg = argv[1];                                                           
+  auto request = std::make_shared<beginner_tutorials::srv::InputStr::Request>();
+  request->inp_msg = argv[1];
 
   while (!client->wait_for_service(1s)) {
     if (!rclcpp::ok()) {
-      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),
+                   "Interrupted while waiting for the service. Exiting.");
       return 0;
     }
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+                "service not available, waiting again...");
   }
 
   auto result = client->async_send_request(request);
   // Wait for the result.
   if (rclcpp::spin_until_future_complete(node, result) ==
-    rclcpp::FutureReturnCode::SUCCESS)
-  {
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "String: %s", result.get()->out_msg.c_str());
+      rclcpp::FutureReturnCode::SUCCESS) {
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "String: %s",
+                result.get()->out_msg.c_str());
   } else {
-    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service InputStr");   
+    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),
+                 "Failed to call service InputStr");
   }
 
   rclcpp::shutdown();
--- src/pub.cpp
+++ src/pub.cpp
@@ -4,18 +4,18 @@
  * @brief A publisher script
  * @version 0.1
  * @date 2022-11-01
- * 
+ *
  * @copyright MIT License (c) 2022 Shailesh Pranav Rajendran
- * 
+ *
  */
 #include <chrono>
 #include <functional>
 #include <memory>
 #include <string>
 
+#include "beginner_tutorials/srv/input_str.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "beginner_tutorials/srv/input_str.hpp"
 
 using namespace std::chrono_literals;
 /* This example creates a subclass of Node and uses std::bind() to register a
@@ -34,26 +34,31 @@ class Pub : public rclcpp::Node {
       RCLCPP_ERROR_STREAM(this->get_logger(), "Publish Frequency changed");
     RCLCPP_INFO_STREAM(this->get_logger(), "Frequency value : " << freq);
 
-    try{
-    service = this->create_service<beginner_tutorials::srv::InputStr>(
-          "service_node", std::bind(&Pub::input_string, this, std::placeholders::_1,
+    try {
+      service = this->create_service<beginner_tutorials::srv::InputStr>(
+          "service_node",
+          std::bind(&Pub::input_string, this, std::placeholders::_1,
                     std::placeholders::_2));
-    }
-    catch(...){
-      RCLCPP_ERROR_STREAM(this->get_logger(),"Error in calling service");
-      RCLCPP_FATAL_STREAM(this->get_logger(),"String might not be changed");
+    } catch (...) {
+      RCLCPP_ERROR_STREAM(this->get_logger(), "Error in calling service");
+      RCLCPP_FATAL_STREAM(this->get_logger(), "String might not be changed");
     }
     timer_ =
-        this->create_wall_timer
-                    (std::chrono::milliseconds(int((1000 / freq))), std::bind(&Pub::timer_callback, this));
+        this->create_wall_timer(
+          std::chrono::milliseconds(static_cast<int>((1000 / freq))),
+          std::bind(&Pub::timer_callback, this));
   }
-  void input_string(const std::shared_ptr<beginner_tutorials::srv::InputStr::Request> request,
-          std::shared_ptr<beginner_tutorials::srv::InputStr::Response> response){
+  void input_string(
+      const std::shared_ptr<beginner_tutorials::srv::InputStr::Request> request,
+      std::shared_ptr<beginner_tutorials::srv::InputStr::Response> response) {
     response->out_msg = request->inp_msg;
     new_str = response->out_msg;
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request: %s", request->inp_msg.c_str());
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sending back response: %s", response->out_msg.c_str());
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request: %s",
+                request->inp_msg.c_str());
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sending back response: %s",
+                response->out_msg.c_str());
   }
+
  private:
   void timer_callback() {
     auto message = std_msgs::msg::String();
@@ -73,11 +78,11 @@ class Pub : public rclcpp::Node {
   size_t count_;
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  std::shared_ptr<rclcpp::Node> node = std::make_shared<Pub>();  
+  std::shared_ptr<rclcpp::Node> node = std::make_shared<Pub>();
   rclcpp::spin(node);
-  RCLCPP_FATAL_STREAM(node->get_logger(),"ROS Shutting Down");
+  RCLCPP_FATAL_STREAM(node->get_logger(), "ROS Shutting Down");
   rclcpp::shutdown();
   return 0;
 }
