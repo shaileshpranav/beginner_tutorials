adb6f9daa3a67c19f48f189e62bc8df0f8a50c98
Shailesh Pranav Rajendran <shaileshpranav.r@gmail.com>
2022-11-16 18:45:21 -0500

    ROS Version rrror Corrected

--- src/sub.cpp
+++ src/sub.cpp
@@ -4,10 +4,11 @@
  * @brief A subscriber script
  * @version 0.1
  * @date 2022-11-01
- * 
+ *
  * @copyright MIT License (c) 2022 Shailesh Pranav Rajendran
- * 
+ *
  */
+#include <memory>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
@@ -17,17 +18,17 @@ class Sub : public rclcpp::Node {
  public:
   Sub() : Node("Subscriber") {
     subscription_ = this->create_subscription<std_msgs::msg::String>(
-        "topic", 10, std::bind(&Sub::topic_callback, this, _1));
+        "chatter", 10, std::bind(&Sub::topic_callback, this, _1));
   }
 
  private:
-  void topic_callback(const std_msgs::msg::String& msg) const {
-    RCLCPP_INFO(this->get_logger(), "From publisher: '%s'", msg.data.c_str());
+  void topic_callback(const std_msgs::msg::String::SharedPtr msg) const {
+    RCLCPP_INFO(this->get_logger(), "From publisher '%s'", msg->data.c_str());
   }
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<Sub>());
   rclcpp::shutdown();
