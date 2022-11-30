2e5ab5321fd38b63e8c26a250fdf846e4d2cde10
Shailesh Pranav Rajendran <shaileshpranav.r@gmail.com>
2022-11-30 18:40:25 -0500

    Added tf

--- src/pub.cpp
+++ src/pub.cpp
@@ -14,16 +14,20 @@
 #include <string>
 
 #include "beginner_tutorials/srv/input_str.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_ros/static_transform_broadcaster.h"
 
 using namespace std::chrono_literals;
 /* This example creates a subclass of Node and uses std::bind() to register a
  * member function as a callback from the timer. */
-
 class Pub : public rclcpp::Node {
  public:
-  Pub() : Node("Publisher"), count_(0) {
+  Pub(char *transformation[]) : Node("Publisher"), count_(0) {
+    tf_static_broadcaster_ =
+        std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
     publisher_ = this->create_publisher<std_msgs::msg::String>("chatter", 10);
 
     auto param_desc = rcl_interfaces::msg::ParameterDescriptor{};
@@ -43,10 +47,10 @@ class Pub : public rclcpp::Node {
       RCLCPP_ERROR_STREAM(this->get_logger(), "Error in calling service");
       RCLCPP_FATAL_STREAM(this->get_logger(), "String might not be changed");
     }
-    timer_ =
-        this->create_wall_timer(
-          std::chrono::milliseconds(static_cast<int>((1000 / freq))),
-          std::bind(&Pub::timer_callback, this));
+    this->make_transforms(transformation);
+    timer_ = this->create_wall_timer(
+        std::chrono::milliseconds(static_cast<int>((1000 / freq))),
+        std::bind(&Pub::timer_callback, this));
   }
   void input_string(
       const std::shared_ptr<beginner_tutorials::srv::InputStr::Request> request,
@@ -69,18 +73,39 @@ class Pub : public rclcpp::Node {
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
+  void make_transforms(char *transformation[]) {
+    geometry_msgs::msg::TransformStamped t;
+
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id = "world";
+    t.child_frame_id = transformation[1];
+
+    t.transform.translation.x = atof(transformation[2]);
+    t.transform.translation.y = atof(transformation[3]);
+    t.transform.translation.z = atof(transformation[4]);
+    tf2::Quaternion q;
+    q.setRPY(atof(transformation[5]), atof(transformation[6]),
+             atof(transformation[7]));
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
+
+    tf_static_broadcaster_->sendTransform(t);
+  }
 
   std::string new_str = "Hey Terps";
   // int freq;
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   rclcpp::Service<beginner_tutorials::srv::InputStr>::SharedPtr service;
+  std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_static_broadcaster_;
   size_t count_;
 };
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  std::shared_ptr<rclcpp::Node> node = std::make_shared<Pub>();
+  std::shared_ptr<rclcpp::Node> node = std::make_shared<Pub>(argv);
   rclcpp::spin(node);
   RCLCPP_FATAL_STREAM(node->get_logger(), "ROS Shutting Down");
   rclcpp::shutdown();
